d808b333dc967eb2025aaf369c24d15a53af3e85
JordanMitchel <jmitchel24@gmail.com>
2023-09-22 10:14:26 +0100

    Add ASV bible and import script into mongo

--- MongoStore.py
+++ MongoStore.py
@@ -20,5 +20,28 @@ def mongoimport(csv_path, db_name, coll_name, db_url='localhost', db_port=27017)
     return coll.count_documents({})
 
 
+def insert_bible_to_mongo(json_path, db_name, coll_name, db_url='localhost', db_port=27017):
+    client = MongoClient(db_url, db_port)
+    db = client[db_name]
+    collection = db[coll_name]
+
+
+    # Loading or Opening the json file
+    with open(json_path) as file:
+        file_data = json.load(file)
+
+    data = file_data["verses"]
+
+# Inserting the loaded data in the Collection
+# if JSON contains data more than one entry
+# insert_many is used else insert_one is used
+    if isinstance(data, list):
+        collection.insert_many(data)
+    else:
+        collection.insert_one(data)
+
 totalCount =mongoimport("Data/biblicalLonLat2_formatted.csv", "bibleLocations", "LonLats")
+
+totalCountBible = insert_bible_to_mongo("Data/asv.json","bibleVersions","ASV")
+print(totalCountBible)
 print(totalCount)
