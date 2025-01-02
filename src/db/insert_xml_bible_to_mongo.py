cdbe5f74fb632386a7e4b6add4195bedc5ae6573
Jordan Mitchel <jmitchel24@gmail.com>
2025-01-02 15:39:36 +0000

    fix pylint issues

--- src/db/insert_xml_bible_to_mongo.py
+++ src/db/insert_xml_bible_to_mongo.py
@@ -1,31 +1,40 @@
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as eT
 from src.db.config import get_database
 
-tree = ET.parse('../../Data/xml/Bible_English_MSG.xml')
 
-bibleXml = tree.findall('BIBLEBOOK')
+async def insert_xml_bible_to_mongo(bible_version:str):
+    tree = eT.parse(f'../../Data/xml/{bible_version}.xml')
+    
+    bible_xml = tree.findall('BIBLEBOOK')
+    
+    version_of_bible = tree.getroot().attrib.get('biblename')[-3:]
+    collection = f"Bible_{version_of_bible}"
+    db =  await get_database()
+    coll = db[collection]
+    
+    for book in bible_xml:
+        # bibleBook = book.find('BIBLEBOOK').text
+        book_name = book.attrib.get("bname")
+        book_number = book.attrib.get("bnumber")
+    
+        chapters = book.findall("CHAPTER")
+        for chapter in chapters:
+            chapter_num = chapter.attrib.get("cnumber")
+            chapter_text = chapter.text
+            verses = chapter.findall("VERS")
+            for verse in verses:
+                verse_num = verse.attrib.get("vnumber")
+                verse_text = verse.text
+    
+                bible_dict = {
+                    "book_name": book_name,
+                    "book": book_number,
+                    "chapter": chapter_num,
+                    "verse": verse_num,
+                    "text": verse_text,
+                }
+                coll.insert_one(bible_dict)
+    
+    print (coll.count())
 
-version_of_bible = tree._root.attrib.get('biblename')[-3:]
-COLLECTION = f"Bible_{version_of_bible}"
-db =  get_database()
-coll = db[COLLECTION]
-
-for book in bibleXml:
-    # bibleBook = book.find('BIBLEBOOK').text
-    book_name = book.attrib.get("bname")
-    book_number = book.attrib.get("bnumber")
-
-    chapters = book.findall("CHAPTER")
-    for chapter in chapters:
-        chapter_num = chapter.attrib.get("cnumber")
-        chapter_text = chapter.text
-        verses = chapter.findall("VERS")
-        for verse in verses:
-            verse_num = verse.attrib.get("vnumber")
-            verse_text = verse.text
-            bible_dict = [
-                dict(book_name=book_name, book=book_number, chapter=chapter_num, verse=verse_num, text=verse_text)
-            ]
-            x = coll.insert(bible_dict)
-
-print (coll.count())
+insert_xml_bible_to_mongo("Bible_English_MSG")
